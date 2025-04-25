5a9673309307c056f2bf07c732f168838e2c9f49
Mohamed Saied <mohameedsaieed47@gmail.com>
2025-04-25 15:57:14 +0300

    modified dashboard_screen

--- lib/admins/dashboard_screen.dart
+++ lib/admins/dashboard_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AdminDashboardScreen extends StatefulWidget {
+  const AdminDashboardScreen({super.key});
+
   @override
+  // ignore: library_private_types_in_public_api
   _AdminDashboardScreenState createState() => _AdminDashboardScreenState();
 }
 
